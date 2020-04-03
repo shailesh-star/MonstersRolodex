60e1c8ef9ea332d527c6b5c88505c828f0e1e5c3
unknown <shailesh.mauryaa83@gmail.com>
2020-04-03 16:38:58 +0530

    Updates

---
+++ precache-manifest.26b1df33c2e7ce9e1b908bbbd04ce04c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10686b5d003a0552103bdc05bc96d885",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "96a559f9dbc35fccd5b5",
+    "url": "/MonstersRolodex/static/css/main.08ff41a5.chunk.css"
+  },
+  {
+    "revision": "4d52e683462906fef46f",
+    "url": "/MonstersRolodex/static/js/2.484117f9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MonstersRolodex/static/js/2.484117f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96a559f9dbc35fccd5b5",
+    "url": "/MonstersRolodex/static/js/main.90051f9d.chunk.js"
+  },
+  {
+    "revision": "f4465fbec44200d28655",
+    "url": "/MonstersRolodex/static/js/runtime-main.68f0dc25.js"
+  }
+]);
